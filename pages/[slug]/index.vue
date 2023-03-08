f72fcdce6656e04e7fbdd92ada6d4f8ae36fa7e5
keuller <keuller.magalhaes@gmail.com>
2023-03-08 09:43:18 +0000

    fix: image placeholder

--- pages/[slug]/index.vue
+++ pages/[slug]/index.vue
@@ -41,7 +41,7 @@
             <div class="flex justify-between items-center bg-white xs:w-full lg:w-[990px] lg:mx-auto rounded-md px-4 py-2 shadow-md">
                 <div class="flex flex-col gap-4">
                     <div class="flex gap-4 items-center">
-                        <img src="people.person.avatar"
+                        <img src="https://placehold.jp/3d4070/ffffff/150x150.png"
                             alt="avatar" width="64" height="64" class="rounded-full h-16 w-16" />
 
                         <div class="flex flex-col tracking-wide">
