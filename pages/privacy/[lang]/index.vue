a55fb208329acf5036757072391cbf9f446ec28d
keuller <keuller.magalhaes@gmail.com>
2023-03-10 14:47:09 +0000

    wip: added terms conditions page

---
+++ pages/privacy/[lang]/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template>
+    <section id="terms" class="relative flex flex-col bg-slate-50">
+        <div class="xs:w-full lg:w-[990px] lg:mx-auto">
+            <h1 class="text-2xl font-bold py-4">Privacy Policy</h1>
+        </div>
+    </section>
+</template>
---
+++ pages/terms-conditions/[lang]/index.vue
@@ -0,0 +1,139 @@
+<script lang="ts"></script>
+
+<template>
+  <section id="terms" class="relative flex flex-col bg-slate-50">
+    <div class="xs:w-full lg:w-[990px] lg:mx-auto">
+      <h1 class="text-2xl font-bold py-4">Terms and conditions</h1>
+      <p class="leading-7 tracking-wide">
+        These terms and conditions (&#8220;Agreement&#8221;) set forth the general terms
+        and conditions of your use of the
+        <a href="https://getrezume.com">getrezume.com</a> website (&#8220;Website&#8221;
+        or &#8220;Service&#8221;) and any of its related products and services
+        (collectively, &#8220;Services&#8221;). This Agreement is legally binding between
+        you (&#8220;User&#8221;, &#8220;you&#8221; or &#8220;your&#8221;) and this Website
+        operator (&#8220;Operator&#8221;, &#8220;we&#8221;, &#8220;us&#8221; or
+        &#8220;our&#8221;). If you are entering into this agreement on behalf of a
+        business or other legal entity, you represent that you have the authority to bind
+        such entity to this agreement, in which case the terms &#8220;User&#8221;,
+        &#8220;you&#8221; or &#8220;your&#8221; shall refer to such entity. If you do not
+        have such authority, or if you do not agree with the terms of this agreement, you
+        must not accept this agreement and may not access and use the Website and
+        Services. By accessing and using the Website and Services, you acknowledge that
+        you have read, understood, and agree to be bound by the terms of this Agreement.
+        You acknowledge that this Agreement is a contract between you and the Operator,
+        even though it is electronic and is not physically signed by you, and it governs
+        your use of the Website and Services.
+      </p>
+      <div class="wpembed-toc">
+        <h3 class="text-xl font-sem py-2">Table of contents</h3>
+        <ol class="wpembed-toc leading-8">
+          <li class="pl-4"><a href="#accounts-and-membership">Accounts and membership</a></li>
+          <li class="pl-4"><a href="#user-content">User content</a></li>
+          <li class="pl-4"><a href="#backups">Backups</a></li>
+          <li class="pl-4"><a href="#links-to-other-resources">Links to other resources</a></li>
+          <li class="pl-4"><a href="#changes-and-amendments">Changes and amendments</a></li>
+          <li class="pl-4"><a href="#acceptance-of-these-terms">Acceptance of these terms</a></li>
+          <li class="pl-4"><a href="#contacting-us">Contacting us</a></li>
+        </ol>
+      </div>
+      <h2 id="accounts-and-membership" class="text-xl font-sem py-3">Accounts and membership</h2>
+      <p class="leading-7 tracking-wide">
+        You must be at least 18 years of age to use the Website and Services. By using the
+        Website and Services and by agreeing to this Agreement you warrant and represent
+        that you are at least 18 years of age. If you create an account on the Website,
+        you are responsible for maintaining the security of your account and you are fully
+        responsible for all activities that occur under the account and any other actions
+        taken in connection with it. We may, but have no obligation to, monitor and review
+        new accounts before you may sign in and start using the Services. Providing false
+        contact information of any kind may result in the termination of your account. You
+        must immediately notify us of any unauthorized uses of your account or any other
+        breaches of security. We will not be liable for any acts or omissions by you,
+        including any damages of any kind incurred as a result of such acts or omissions.
+        We may suspend, disable, or delete your account (or any part thereof) if we
+        determine that you have violated any provision of this Agreement or that your
+        conduct or content would tend to damage our reputation and goodwill. If we delete
+        your account for the foregoing reasons, you may not re-register for our Services.
+        We may block your email address and Internet protocol address to prevent further
+        registration.
+      </p>
+      <h2 id="user-content" class="text-xl font-sem py-3">User content</h2>
+      <p class="leading-7 tracking-wide">
+        We do not own any data, information or material (collectively,
+        &#8220;Content&#8221;) that you submit on the Website in the course of using the
+        Service. You shall have sole responsibility for the accuracy, quality, integrity,
+        legality, reliability, appropriateness, and intellectual property ownership or
+        right to use of all submitted Content. We may, but have no obligation to, monitor
+        and review the Content on the Website submitted or created using our Services by
+        you. You grant us permission to access, copy, distribute, store, transmit,
+        reformat, display and perform the Content of your user account solely as required
+        for the purpose of providing the Services to you. You also grant us the license to
+        use, reproduce, adapt, modify, publish or distribute the Content created by you or
+        stored in your user account for commercial, marketing or any similar purpose.
+      </p>
+      <h2 id="backups" class="text-xl font-sem py-3">Backups</h2>
+      <p class="leading-7 tracking-wide">
+        We perform regular backups of the Website and its Content, however, these backups
+        are for our own administrative purposes only and are in no way guaranteed. You are
+        responsible for maintaining your own backups of your data. We do not provide any
+        sort of compensation for lost or incomplete data in the event that backups do not
+        function properly. We will do our best to ensure complete and accurate backups,
+        but assume no responsibility for this duty.
+      </p>
+      <h2 id="links-to-other-resources" class="text-xl font-sem py-2">
+        Links to other resources
+      </h2>
+      <p class="leading-7 tracking-wide">
+        Although the Website and Services may link to other resources (such as websites,
+        mobile applications, etc.), we are not, directly or indirectly, implying any
+        approval, association, sponsorship, endorsement, or affiliation with any linked
+        resource, unless specifically stated herein. We are not responsible for examining
+        or evaluating, and we do not warrant the offerings of, any businesses or
+        individuals or the content of their resources. We do not assume any responsibility
+        or liability for the actions, products, services, and content of any other third
+        parties. You should carefully review the legal statements and other conditions of
+        use of any resource which you access through a link on the Website. Your linking
+        to any other off-site resources is at your own risk.
+      </p>
+      <h2 id="changes-and-amendments" class="text-xl font-sem py-2">
+        Changes and amendments
+      </h2>
+      <p class="leading-7 tracking-wide">
+        We reserve the right to modify this Agreement or its terms related to the Website
+        and Services at any time at our discretion. When we do, we will send you an email
+        to notify you. We may also provide notice to you in other ways at our discretion,
+        such as through the contact information you have provided.
+      </p>
+      <p class="leading-7 tracking-wide">
+        An updated version of this Agreement will be effective immediately upon the
+        posting of the revised Agreement unless otherwise specified. Your continued use of
+        the Website and Services after the effective date of the revised Agreement (or
+        such other act specified at that time) will constitute your consent to those
+        changes.
+      </p>
+      <h2 id="acceptance-of-these-terms" class="text-xl font-sem py-2">
+        Acceptance of these terms
+      </h2>
+      <p class="leading-7 tracking-wide">
+        You acknowledge that you have read this Agreement and agree to all its terms and
+        conditions. By accessing and using the Website and Services you agree to be bound
+        by this Agreement. If you do not agree to abide by the terms of this Agreement,
+        you are not authorized to access or use the Website and Services. This terms and
+        conditions policy was created with the help of
+        <a href="https://www.websitepolicies.com" target="_blank" rel="nofollow"
+          >WebsitePolicies</a
+        >.
+      </p>
+      <h2 id="contacting-us" class="text-xl font-sem py-2">Contacting us</h2>
+      <p class="leading-7 tracking-wide">
+        If you have any questions, concerns, or complaints regarding this Agreement, we
+        encourage you to contact us using the details below:
+      </p>
+      <p>
+        <a class="text-slate-500 tracking-wider"
+          href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#107;eul&#108;&#101;r.mag&#97;&#108;&#104;a&#101;s&#64;&#103;m&#97;&#105;&#108;&#46;com"
+          >&#107;eu&#108;l&#101;r&#46;m&#97;ga&#108;h&#97;es&#64;g&#109;&#97;il.co&#109;</a>
+      </p>
+      <p class="py-4 text-base text-slate-500">This document was last updated on March 10, 2023</p>
+    </div>
+  </section>
+</template>
