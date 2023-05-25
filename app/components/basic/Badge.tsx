8a02b76045089e11c1eb47d29069e63ea098344d
keuller <keuller.magalhaes@gmail.com>
2023-05-25 16:40:15 +0100

    fix: Badge - removed v-once attribute.

--- app/components/basic/Badge.tsx
+++ app/components/basic/Badge.tsx
@@ -5,7 +5,7 @@ export interface BadgeProps {
 
 export function Badge({ name }: BadgeProps) {
     return (
-        <span v-once className="text-xs tracking-wide rounded-xl bg-primary-light px-3 py-1">
+        <span className="text-xs tracking-wide rounded-xl bg-primary-light px-3 py-1">
             {name}
         </span>
     )
